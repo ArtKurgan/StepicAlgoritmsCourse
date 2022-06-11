525b06f482371ff9101388a4d04cfdcf25483be1
ArtKurgan <artmail937@yahoo.com>
2022-06-11 19:17:09 +0300

    cc

--- 2 week Greedy Algs/FullBag.py
+++ 2 week Greedy Algs/FullBag.py
@@ -4,17 +4,12 @@ sortitems = sorted(items, key=lambda x: x[0]/x[1], reverse=True)
 curWei = 0
 curVal = 0
 for i in sortitems:
-    if curWei == W:
-        break
     if i[1] + curWei <= W:
         curWei += i[1]
         curVal += i[0]
     elif i[1] + curWei > W:
-        if curWei == 0:
-            curVal = i[0] / i[1] * W
-        else:
-            curWei += i[1]
-            curVal += i[0] / i[1] * (i[1] - abs(curWei - W))
+        curWei += i[1]
+        curVal += i[0] / i[1] * (i[1] - abs(curWei - W))
         break
 print(curVal)
 
