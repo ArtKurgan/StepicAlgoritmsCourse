7b88cead5fc9445be1aad9c1747f76a96dc72fd2
ArtKurgan <106882467+ArtKurgan@users.noreply.github.com>
2022-06-17 11:30:42 +0300

    Update KUCHA.py

--- KUCHA.py
+++ KUCHA.py
@@ -1,17 +1,21 @@
 def siftup(i):
-    while i > 0 and kucha[i//2] < kucha[i]:
-       kucha[i], kucha [i//2] = kucha[i//2], kucha[i]
-       i = i//2
+    while i > 0 and kucha[i//2-1] < kucha[i]:
+       if i <= 2:
+           if kucha[i] > kucha[0]:
+               kucha[i], kucha [0] = kucha[0], kucha[i]
+       else:    
+           kucha[i], kucha [i//2-1] = kucha[i//2-1], kucha[i]
+           i = i//2-1
 def sifdown(i):
-    while 2 * i <= len(kucha) and len(kucha) > 1:
+    while 2 * i + 1 <= len(kucha) - 1:
         big = i
-        if kucha[2 * i] > kucha[big]:
-            big = 2*i
-        if 2*i+1 <= len(kucha) and kucha[2*i+1] > kucha[big] and len(kucha) > 2:
-            big = 2*i+1
+        if kucha[2*i+1] > kucha[big]:
+            big = 2 * i
+        if 2 * i + 2 <= len(kucha) - 1 and kucha[2*i+2] > kucha[big]:
+            big = 2*i+2
         if i == big:
             break
-
+            
 n = int(input())
 cnt = -1
 kucha = []
