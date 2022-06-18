fd9ef2e313386e3c46fdeb260bf9b83af258c4af
ArtKurgan <artmail937@yahoo.com>
2022-06-18 19:17:53 +0300

    add huffman

--- week_3/BinareSearch.py
+++ week_3/BinareSearch.py
@@ -1,11 +1,20 @@
+def okrug():
+    x = (l + r) / 2
+    if x < 1:
+        x = 0
+    else:
+        x = round((l + r) / 2)
+    return(x)
+
 n, *A = map(int, input().split())
 k, *nums = map(int, input().split())
 l = 0
-r = n
+r = n-1
 res = []
+m = 0
 for i in range(k):
-    while l <= r:
-        m = round(((l+r)/2))
+    while l <= r and m <= len(A)-1:
+        m = (l + r) // 2
         if A[m] == nums[i]:
             res.append((A.index(nums[i])+1))
             break
@@ -16,5 +25,5 @@ for i in range(k):
     if len(res) < i+1:
         res.append(-1)
     l = 0
-    r = n
+    r = n-1
 print(*res)
