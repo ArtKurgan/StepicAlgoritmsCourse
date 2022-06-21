b42098c184f01947db5480d5553ed764657ee188
ArtKurgan <artmail937@yahoo.com>
2022-06-21 23:19:28 +0300

    add huffman

--- week_3/MergeSort.py
+++ week_3/MergeSort.py
@@ -1,19 +1,20 @@
 n = int(input())
 array_input = list(map(int, input().split()))
-
+inversion = 0
 
 def separation(array, left_edge, right_edge):
-    while left_edge < right_edge:
-        # if len(array) == 1:
-        #     return array
-        middle = (left_edge + right_edge) // 2
-        array_left = array[0:middle+1]
-        array_right = array[middle+1:len(array)]
-        merge(separation(array_left, 0, middle), separation(array_right, middle+1, len(array)-1))
+        if len(array) > 1:
+            print(array)
+            middle = (left_edge + right_edge) // 2
+            array_left = array[0:middle]
+            array_right = array[middle:len(array)]
+            return merge(separation(array_left, 0, middle), separation(array_right, middle+1, len(array_right)-1))
+        return array
 
 
 def merge(array_1, array_2):
     sorted_array = []
+    inversion = 0
     for i in range(len(array_1) + len(array_2)):
         if len(array_1) == 0:
             sorted_array= sorted_array + array_2
@@ -23,10 +24,11 @@ def merge(array_1, array_2):
             break
         if array_1[0] > array_2[0]:
             sorted_array.append(array_2.pop(0))
-        elif array_1[0] < array_2[0]:
+            inversion += 1
+        elif array_1[0] <= array_2[0]:
             sorted_array.append(array_1.pop(0))
+            inversion += 1
     return sorted_array
 
 
-separation(array_input, 0, n-1)
-print(array_input)
+print(separation(array_input, 0, n-1))
