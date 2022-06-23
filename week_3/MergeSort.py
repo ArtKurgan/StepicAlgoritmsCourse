c2bc3fa3d1111dc54fcba07e5b90f2ea996062ee
ArtKurgan <artmail937@yahoo.com>
2022-06-23 19:36:14 +0300

    add huffman

--- week_3/MergeSort.py
+++ week_3/MergeSort.py
@@ -4,7 +4,6 @@ inversion = 0
 
 def separation(array, left_edge, right_edge):
         if len(array) > 1:
-            print(array)
             middle = (left_edge + right_edge) // 2
             array_left = array[0:middle]
             array_right = array[middle:len(array)]
@@ -14,21 +13,21 @@ def separation(array, left_edge, right_edge):
 
 def merge(array_1, array_2):
     sorted_array = []
-    inversion = 0
+    global inversion
     for i in range(len(array_1) + len(array_2)):
         if len(array_1) == 0:
-            sorted_array= sorted_array + array_2
+            sorted_array = sorted_array + array_2
             break
         elif len(array_2) == 0:
             sorted_array = sorted_array + array_1
             break
         if array_1[0] > array_2[0]:
             sorted_array.append(array_2.pop(0))
-            inversion += 1
+            inversion += len(array_1)
         elif array_1[0] <= array_2[0]:
             sorted_array.append(array_1.pop(0))
-            inversion += 1
     return sorted_array
 
 
-print(separation(array_input, 0, n-1))
+separation(array_input, 0, n-1)
+print(inversion)
